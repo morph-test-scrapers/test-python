421d07210544d88702ee6d9ff1137e5d1301d10b
Matthew Landauer <matthew@oaf.org.au>
2015-04-08 04:05:10 +1000

    Add loop to test output streaming

--- scraper.py
+++ scraper.py
@@ -1,4 +1,9 @@
 import scraperwiki
+import time
 
 # Write out to the sqlite database using scraperwiki library
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+
+for i in range(1, 6):
+    print "%i..." % i
+    time.sleep(1)
