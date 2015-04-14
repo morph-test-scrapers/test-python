f87e8715f2009096618d4113fdbfaf27e9a7df9c
Matthew Landauer <matthew@oaf.org.au>
2015-04-14 10:34:25 +1000

    Add checking an ssl site

--- scraper.py
+++ scraper.py
@@ -1,6 +1,14 @@
 import scraperwiki
 import time
 
+# Check that proxy gives a working certificate for SSL connections
+# If the certificate isn't valid it should throw an exception
+
+html = scraperwiki.scrape("https://morph.io")
+
+if not "Get structured data out of the web" in html:
+    raise Exception("Not expected result")
+
 # Write out to the sqlite database using scraperwiki library
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
