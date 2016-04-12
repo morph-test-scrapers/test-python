afa336a8d2802322efc050deff24271225151a04
Henare Degan <henare.degan@gmail.com>
2016-04-12 10:48:32 +1000

    Oops missed a spot

--- scraper.py
+++ scraper.py
@@ -2,17 +2,18 @@ import scraperwiki
 import time
 import requests
 
+expected_string = "Hassle-free web scraping."
+
 # Check that proxy gives a working certificate for SSL connections
 # If the certificate isn't valid it should throw an exception
-
 html = scraperwiki.scrape("https://morph.io")
 
-if not "Hassle-free web scraping." in html:
+if not expected_string in html:
     raise Exception("Not expected result")
 
 # Use requests library to do the same because it gets its CA certs a different way. Oh joy.
 r = requests.get('https://morph.io')
-if not "Get structured data out of the web" in r.text:
+if not expected_string in r.text:
     raise Exception("Not expected result")
 
 # Write out to the sqlite database using scraperwiki library
